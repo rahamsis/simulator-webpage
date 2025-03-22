2640066b73005394769ad3bb1244bf60ede08ca5
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-03-22 00:48:09 -0500

    deleting logger

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-require("./_logger.js");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  logger: {
-    log: (message) => console.log(`[${new Date().toISOString()}] ${message}`),
-    error: (message) => console.error(`[${new Date().toISOString()}] ERROR: ${message}`),
-    warn: (message) => console.warn(`[${new Date().toISOString()}] WARNING: ${message}`),
-  },
   webpack: (config, { isServer }) => {
     // Configura Webpack para manejar archivos .mjs
     config.module.rules.push({
