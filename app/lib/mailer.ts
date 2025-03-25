543693558af306cb4362d96ae198bb07f7e9f547
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-03-24 20:48:34 -0500

    adding double verificatino

--- app/lib/mailer.ts
+++ app/lib/mailer.ts
@@ -7,14 +7,33 @@ export async function sendVerificationEmail(email: string, token: string) {
             auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
         });
 
-        const verifyUrl = `${process.env.NEXTAUTH_URL || "http://localhost:3000" }/api/verify?sessionToken=${token}`;
+        const verifyUrl = `${process.env.NEXTAUTH_URL || "http://localhost:3000"}/api/verify?sessionToken=${token}`;
+
+        const currentYear = new Date().getFullYear()
 
         await transporter.sendMail({
             from: 'metodoponte100@gmail.com',
             to: email,
             subject: "Verifica tu cuenta",
-            html: `<p>Haz clic en el siguiente enlace para verificar tu cuenta:</p>
-               <a href="${verifyUrl}">Verificar cuenta</a>`,
+            html: `<html>
+                        <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; text-align: center;">
+                            <div style="max-width: 500px; background: #ffffff; padding: 20px; margin: auto; border-radius: 10px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);">
+                                <h2 style="color: #333;">Verificación de Cuenta</h2>
+                                <p style="color: #555;">¡Gracias por registrarte! Para activar tu cuenta, haz clic en el botón de abajo.</p>
+                                <a href="${verifyUrl}" 
+                                    style="display: inline-block; padding: 12px 20px; font-size: 16px; color: #fff; background-color: #338e30; text-decoration: none; border-radius: 5px; margin-top: 10px;">
+                                    ✅ Verificar Cuenta
+                                </a>
+                                <p style="margin-top: 20px; font-size: 14px; color: #777;">
+                                    Si no solicitaste esta verificación, ignora este mensaje.
+                                </p>
+                                <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;">
+                                <p style="font-size: 12px; color: #aaa;">
+                                    ® ${currentYear} Ponte 100 | Todos los derechos reservados.
+                                </p>
+                            </div>
+                        </body>
+                    </html>`,
         });
     } catch (error) {
         console.error("❌ Error enviando correo de verificación:", error);
