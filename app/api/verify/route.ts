b427aa4b223c39dc057dd2e759c7ec2992566c6d
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-03-24 21:33:09 -0500

    adding double verificatino

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,18 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from "next/server";
 import { fetchActiveSession } from '@/app/lib/data';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const { token } = req.query;
+export async function GET(req: NextRequest) {
+    const url = new URL(req.url);
+    const sessionToken = url.searchParams.get("sessionToken");
 
-    if (!token) {
-        return res.status(400).json({ message: "Token inválido" });
+    if (!sessionToken) return NextResponse.json({ error: "Token inválido" }, { status: 401 });
+
+    try {
+        const user = await fetchActiveSession(sessionToken as string);
+
+        if (!user || user.length === 0) {
+            return NextResponse.json({ message: "Token no válido o expirado." }, { status: 400 });
+        }
+
+        return NextResponse.redirect(new URL("/login?verified=true", req.url)); // Redirige a login después de verificar
+    } catch (error) {
+        console.error("Error al obtener la sesión:", error);
+        return NextResponse.json({ error: "Error interno" }, { status: 500 });
     }
-
-    const user = await fetchActiveSession(token as string);
-
-    if (!user || user.length === 0) {
-        return res.status(400).json({ message: "Token no válido o expirado." });
-    }
-
-    res.redirect('/login?verified=true'); // Redirige a login después de verificar
 }
